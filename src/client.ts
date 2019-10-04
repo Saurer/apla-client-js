44d597372dd52337ac21df8dcc17d7e888d1183a
Frank Horrigan <redmmond@gmail.com>
2019-10-05 03:31:57 +1000

    Update client to new arch, add test coverage

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,24 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) EGAAS S.A. All rights reserved.
-*  See LICENSE in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) EGAAS S.A. All rights reserved.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 import urlJoin from 'url-join';
-import Endpoint, { EndpointRequestType, AnyEndpoint, EndpointResponseType, ResponseType } from './endpoint';
+import queryString from 'query-string';
 import { Middleware } from './endpoint/middleware';
+import { NetworkError } from './types/error';
+import Endpoint, {
+    EndpointRequestType,
+    AnyEndpoint,
+    EndpointResponseType,
+    ResponseType,
+    EndpointMethod
+} from './endpoint';
+import urlTemplate from 'url-template';
 
 export interface ApiOptions {
+    transport: IRequestTransport;
     apiEndpoint?: string;
-    transport?: IRequestTransport;
     headers?: {
         [key: string]: string;
     };
@@ -26,26 +35,20 @@ export default abstract class Client {
 
     constructor(apiHost: string, options: ApiOptions) {
         this.apiHost = apiHost;
-        this.options = {
-            ...options,
-            headers: options.headers || {},
-            middleware: options.middleware || []
-        };
+        this.options = options;
     }
 
-    protected serializeUrlSearchParams = (values: { [key: string]: Array<object> | object }) => {
+    protected serializeUrlSearchParams = (values: {
+        [key: string]: Array<object> | object;
+    }) => {
         const params = new URLSearchParams();
         for (let key in values) {
             const value = values[key];
             if (undefined === value || null === value) {
                 continue;
-            }
-            else if (Array.isArray(value)) {
-                value.forEach(subValue =>
-                    params.append(key, String(subValue))
-                );
-            }
-            else {
+            } else if (Array.isArray(value)) {
+                value.forEach(subValue => params.append(key, String(subValue)));
+            } else {
                 params.append(key, String(value));
             }
         }
@@ -53,7 +56,24 @@ export default abstract class Client {
         return params;
     };
 
+    protected serializeQueryString = (values: { [key: string]: object }) => {
+        return queryString.stringify(values, {
+            arrayFormat: 'comma'
+        });
+    };
+
+    protected serializeSlug = (
+        template: string,
+        params: { [key: string]: object }
+    ) => {
+        return urlTemplate.parse(template).expand(params);
+    };
+
     protected executeMiddleware = (response: any) => {
+        if (!this.options.middleware) {
+            return response;
+        }
+
         let result = response;
         for (let handler of this.options.middleware) {
             result = handler(result);
@@ -62,60 +82,97 @@ export default abstract class Client {
         return result;
     };
 
-    protected request = async <TResponse, TRequest>(endpoint: Endpoint<TResponse, TRequest>, params?: TRequest) => {
-        const routeEndpoint = endpoint.getRoute(params);
-        const requestUrl = urlJoin(this.apiHost, this.options.apiEndpoint, routeEndpoint.route, routeEndpoint.query);
-        const requestParams = endpoint.serialize(params);
+    protected shouldUseQueryParams = (method: EndpointMethod) => {
+        return EndpointMethod.Get === method;
+    };
 
-        // TODO: NetworkError exception
-        const response = await this.options.transport(requestUrl, {
-            method: requestParams.method,
-            mode: 'cors',
-            headers: this.options.headers,
-            body: ['get', 'head'].indexOf(requestParams.method.toLowerCase()) === -1 ?
-                this.serializeUrlSearchParams(requestParams.form) :
-                null
-        });
+    protected request = async <TResponse, TRequest>(
+        endpoint: Endpoint<TResponse, TRequest>,
+        params: TRequest | void
+    ) => {
+        const useQuery = this.shouldUseQueryParams(endpoint.method);
+        const request = params ? endpoint.serialize(params) : null;
+        const route = request
+            ? this.serializeSlug(endpoint.route, request.slug)
+            : endpoint.route;
+        const query =
+            useQuery && request
+                ? '?' + this.serializeQueryString(request.body)
+                : '';
+        const body =
+            !useQuery && request
+                ? this.serializeUrlSearchParams(request.body)
+                : null;
+        const requestUrl = urlJoin(
+            this.apiHost,
+            this.options.apiEndpoint || '',
+            route,
+            '?' === query ? '' : query
+        );
 
-        let json: any = null;
-        let plainText: string | null = null;
+        try {
+            const response = await this.options.transport(requestUrl, {
+                method: endpoint.method,
+                mode: 'cors',
+                headers: this.options.headers,
+                body
+            });
 
-        switch (requestParams.responseType) {
-            case ResponseType.Both:
-                plainText = await response.clone().text();
-                json = await response.json();
-                break;
+            let json: any = null;
+            let plainText: string = '';
 
-            case ResponseType.PlainText:
-                plainText = await response.text();
-                break;
+            switch (endpoint.responseType) {
+                case ResponseType.Both:
+                    plainText = await response.clone().text();
+                    json = await response.json();
+                    break;
 
-            case ResponseType.Json:
-                json = await response.json();
-                break;
+                case ResponseType.PlainText:
+                    plainText = await response.text();
+                    break;
+
+                case ResponseType.Json:
+                    json = await response.json();
+                    break;
+            }
+
+            const middlewareResult = this.executeMiddleware(json);
+            return request
+                ? request.getResponse(middlewareResult, plainText)
+                : undefined;
+        } catch (e) {
+            throw new NetworkError(e);
         }
-
-        const middlewareResult = this.executeMiddleware(json);
-        return requestParams.getResponse(middlewareResult, plainText);
     };
 
     protected endpoint = <TResponse>(endpoint: Endpoint<TResponse>) => {
         return () => this.request(endpoint) as Promise<TResponse>;
-    }
+    };
 
-    protected parametrizedEndpoint = <TEndpoint extends AnyEndpoint, TDefaults extends Partial<EndpointRequestType<TEndpoint>>>(
+    protected parametrizedEndpoint = <
+        TEndpoint extends AnyEndpoint,
+        TDefaults extends Partial<EndpointRequestType<TEndpoint>>
+    >(
         endpoint: TEndpoint,
         defaultParams?: TDefaults
     ) => {
-        return (params: EndpointRequestWithDefaults<EndpointRequestType<TEndpoint>, TDefaults>) =>
+        return (
+            params: EndpointRequestWithDefaults<
+                EndpointRequestType<TEndpoint>,
+                TDefaults
+            >
+        ) =>
             this.request(endpoint, {
                 ...defaultParams,
                 ...params
             }) as Promise<EndpointResponseType<TEndpoint>>;
-    }
+    };
 }
 
-type EndpointRequestWithDefaults<TRequest, TDefaults = {}> =
-    Pick<TRequest, Exclude<keyof TRequest, keyof TDefaults>> & {
+type EndpointRequestWithDefaults<TRequest, TDefaults = {}> = Pick<
+    TRequest,
+    Exclude<keyof TRequest, keyof TDefaults>
+> &
+    {
         [D in keyof TDefaults]?: TDefaults[D];
-    }
+    };
