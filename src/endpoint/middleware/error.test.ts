baca389b8d64acf018362a104257b3ac2bec5776
Frank Horrigan <redmmond@gmail.com>
2019-10-04 21:32:37 +1000

    Remove commented code

--- src/endpoint/middleware/error.test.ts
+++ src/endpoint/middleware/error.test.ts
@@ -63,22 +63,5 @@ describe('Error middleware', () => {
         );
     });
 });
-// import { Middleware } from '.';
-
-// const errorMiddleware: Middleware = response => {
-//     if (!response || 'object' !== typeof response) {
-//         return response;
-//     }
-
-//     if (!('error' in response && 'msg' in response)) {
-//         return response;
-//     }
-
-//     throw new APIError(
-//         String(response.error),
-//         String(response.msg),
-//         response.params
-//     );
-// };
 
 export default errorMiddleware;
