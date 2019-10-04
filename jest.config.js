9483879363153960361d04671749d70fa026823e
Frank Horrigan <redmmond@gmail.com>
2019-10-04 20:45:10 +1000

    Update base configuration & dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,9 @@ const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$';
 module.exports = {
     testRegex: TEST_REGEX,
     testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-    moduleFileExtensions: ['js', 'ts'],
     collectCoverage: true,
     moduleDirectories: ['node_modules', 'src'],
-    setupFilesAfterEnv: [
-        '<rootDir>/src/setupTests.ts'
-    ],
-    roots: [
-        '<rootDir>/src'
-    ],
-    transform: {
-        "^.+\\.tsx?$": 'ts-jest'
-    }
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    testEnvironment: 'node'
 };
