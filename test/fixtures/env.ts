afc7837262971089c488d807ffc6fa6e8702cb00
Frank Horrigan <redmmond@gmail.com>
2019-10-07 13:47:48 +1000

    Remove unused tests and features

--- test/fixtures/env.ts
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) EGAAS S.A. All rights reserved.
-*  See LICENSE in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
-
-export const apiHost = process.env.API_HOST || 'http://127.0.0.1:7079';
