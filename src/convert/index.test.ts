7bef97b629815006ffbfd58606a3c19bbaa0d948
Frank Horrigan <redmmond@gmail.com>
2019-10-10 15:43:08 +1000

    Add convert functions, tests

--- src/convert/index.test.ts
+++ src/convert/index.test.ts
@@ -1,4 +1,11 @@
-import { toHex, toUint8Array } from '.';
+import {
+    toHex,
+    toUint8Array,
+    concatBuffer,
+    encodeLength,
+    encodeLengthPlusData,
+    toMoney
+} from '.';
 
 class MockTextEncoder {
     encode = async (data: string) => {
@@ -38,5 +45,57 @@ describe('Converter', () => {
         const str = arr.map(l => String.fromCharCode(l)).join('');
 
         expect(str).toEqual('4 8 15 16 23 42');
+    });
+
+    it('Should concat two arrays with deterministic result', () => {
+        const a = new Uint8Array([4, 8, 15]);
+        const b = new Uint8Array([16, 23, 42]);
+        const result = concatBuffer(a, b);
+
+        expect(result).toEqual(new Uint8Array([4, 8, 15, 16, 23, 42]));
+    });
+
+    it('Should correctly encode length', () => {
+        expect(encodeLength(32)).toEqual(new Uint8Array([32]));
+        expect(encodeLength(64)).toEqual(new Uint8Array([64]));
+        expect(encodeLength(128)).toEqual(new Uint8Array([129, 128]));
+        expect(encodeLength(256)).toEqual(new Uint8Array([130, 1, 0]));
+        expect(encodeLength(512)).toEqual(new Uint8Array([130, 2, 0]));
+        expect(encodeLength(1024)).toEqual(new Uint8Array([130, 4, 0]));
+    });
+
+    it('Should correctly encode length with data', async () => {
+        const data = await toUint8Array('Hello World!');
+
+        expect(encodeLengthPlusData(data)).toEqual(
+            concatBuffer(encodeLength(data.length), data)
+        );
+    });
+
+    it('Should correctly convert money', () => {
+        expect(toMoney('0.000000000000000128')).toBe('128');
+        expect(toMoney('0.000000000000001024')).toBe('1024');
+        expect(toMoney('0.000000000042853023')).toBe('42853023');
+        expect(toMoney('12.000050080000000128')).toBe('12000050080000000128');
+        expect(toMoney('0')).toBe('0');
+        expect(toMoney('4815162342')).toBe('4815162342000000000000000000');
+        expect(toMoney('12.000050080000000000')).toBe('12000050080000000000');
+        expect(toMoney('0.0000000000000000001')).toBe('0.1');
+    });
+
+    it('Should correctly override money power', () => {
+        expect(toMoney('0.128', 3)).toBe('128');
+        expect(toMoney('1.024', 3)).toBe('1024');
+        expect(toMoney('42853.023', 3)).toBe('42853023');
+        expect(toMoney('12000050080000000.128', 3)).toBe(
+            '12000050080000000128'
+        );
+        expect(toMoney('0', 3)).toBe('0');
+        expect(toMoney('4815162342', 3)).toBe('4815162342000');
+        expect(toMoney('0.0001', 3)).toBe('0.1');
+    });
+
+    it('Should return null if passed money argument is incorrect', () => {
+        expect(toMoney('abc')).toBe(null);
     });
 });
--- src/convert/index.ts
+++ src/convert/index.ts
@@ -3,6 +3,10 @@
  *  See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { Uint64BE } from 'int64-buffer';
+
+export const MONEY_POWER = 18;
+
 export const toHex = (buffer: ArrayBuffer): string => {
     return Array.prototype.map
         .call(new Uint8Array(buffer), (x: number) =>
@@ -21,4 +25,65 @@ export const toUint8Array = async (data: string) => {
         const encoder = new TextEncoder();
         return encoder.encode(data);
     }
+};
+
+export const concatBuffer = (a: Uint8Array, b: Uint8Array) => {
+    const uint8 = new Uint8Array(a.length + b.length);
+
+    uint8.set(a, 0);
+    uint8.set(b, a.length);
+
+    return uint8;
+};
+
+export const encodeLength = (length: number): Uint8Array => {
+    if (length >= 0 && length < 128) {
+        const value = new Uint8Array(1);
+        value[0] = length;
+        return value;
+    }
+
+    const buffer = new Uint64BE(length).toArrayBuffer();
+    const bufferArray = new Uint8Array(buffer);
+
+    let i = 0;
+    while (bufferArray[i] === 0 && i < bufferArray.length) {
+        i++;
+    }
+
+    const offset = 128 | (bufferArray.length - i);
+    const resultArray = bufferArray.slice(i - 1, bufferArray.length);
+    resultArray[0] = offset;
+
+    return resultArray;
+};
+
+export const encodeLengthPlusData = (buffer: Uint8Array): ArrayBuffer => {
+    return concatBuffer(encodeLength(buffer.length), buffer);
+};
+
+export const toMoney = (value: number | string, power = MONEY_POWER) => {
+    const match = /([\d]+)((\.|,)([\d]+))?/.exec(String(value));
+
+    if (!match) {
+        return null;
+    }
+
+    const integer = match[1];
+    const fraction = match[4] || '';
+    let result = integer;
+
+    for (let i = 0; i < power; i++) {
+        const val = fraction.length <= i ? '0' : fraction[i];
+        result += val;
+    }
+
+    if (fraction.length > power) {
+        result =
+            result + '.' + fraction.slice(power, power * 2).replace(/0+$/, '');
+    }
+
+    result = result.replace(/^0+/, '');
+
+    return '' === result || result.startsWith('.') ? '0' + result : result;
 };
